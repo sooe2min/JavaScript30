94e43ba757e79567b109e51087e3567b3153e7d6
Soomin Jang <jsmsumin2@gmail.com>
2022-05-13 06:50:09 +0900

    the end

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -26,6 +26,7 @@ function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60)
   const remainderSeconds = seconds % 60
   const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
+  document.title = display
   timerDisplay.textContent = display
 }
 
